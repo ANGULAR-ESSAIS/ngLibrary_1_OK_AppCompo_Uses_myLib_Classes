2292c9470803327944160e680c8dbd88f5a60c1c
Richard PYT <rick.info.rsoc@gmail.com>
2020-04-26 15:59:16 +0200

    Version nettoyée.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,27 @@
 import { Component, OnInit,
+
          AfterViewInit, ViewChild,
-         ChangeDetectorRef, ChangeDetectionStrategy
+         ChangeDetectorRef, ChangeDetectionStrategy // Juste pour une histoire de ExpressionChangedAfterItHasBeenCheckedError
+                                                    // car modif. par code, du Compo(de ma lib), après son ngOnInit.
        } from '@angular/core';
 
 import {Item, MyNgLibComponent} from 'my-ng-lib';
-// import * as XX from 'my-ng-lib/lib/modeles/Item';
-// import { MyNgLibComponent } from 'my-ng-lib/lib/my-ng-lib.component';
-
-// import * as XX from './../../dist/my-ng-lib/fesm2015/my-ng-lib.js'; //OK mais bof...
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  // changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent implements OnInit, AfterViewInit {
-  //  oItem: XX.Item;
-  //  oChildItem: XX.Item;
    oItem: Item;
    oChildItem: Item;
 
-  //  @ViewChild(XX.MyNgLibComponent, {static: true})
-  //  private oMyNgLibComponent: XX.MyNgLibComponent;
    @ViewChild(MyNgLibComponent, {static: true})
    private oMyNgLibComponent: MyNgLibComponent;
 
   constructor(private oChangeDetectorRef: ChangeDetectorRef) { }
 
   ngOnInit() {
-    // this.oItem = new XX.Item('2', 'MAMA');
     this.oItem = new Item('2', 'MAMA');
     window.setTimeout(() => {
       this.oItem.setLabel('MUMU');
@@ -40,8 +32,8 @@ export class AppComponent implements OnInit, AfterViewInit {
     window.setTimeout(() => {
       this.oChildItem = this.oMyNgLibComponent.getItem();
       this.oChildItem.setLabel('MOMO');
-      this.oChangeDetectorRef.detectChanges(); // Sinon Error : ExpressionChangedAfterItHasBeenCheckedError
-    },1500);
+      this.oChangeDetectorRef.detectChanges(); // << Sinon Error : ExpressionChangedAfterItHasBeenCheckedError
+    }, 1500);
   }
 
 }
